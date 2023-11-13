395c71b9a8266df6b463ae1c828d8f4d2ea0e06a
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-13 10:55:12 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CoverYourAss
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CoverYourAss
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
