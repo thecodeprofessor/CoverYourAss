68acd8ca3604543182634e7980ee53be1802985e
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-14 12:07:40 -0500

    Create a task class to act as a blueprint that defines what a task is.

---
+++ Task.cs
@@ -0,0 +1,9 @@
+﻿namespace CoverYourAss
+{
+    // Task class representing each task, this is our blueprint of what a task is.
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
