4e85ba3e3be025797b476249c7153258abe28f7b
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-14 12:09:50 -0500

    Revised the MainPage UI to display a list of sample tasks and added a button that can be used in the future to add a new task.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,38 @@
-﻿namespace CoverYourAss
+﻿using System.Collections.ObjectModel;
+
+namespace CoverYourAss
 {
+    // MainPage class of the application, inheriting from ContentPage.
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        // A collection that can be observed for changes. Used here to hold Task items.
+        public ObservableCollection<Task> Tasks { get; set; }
 
+        // Constructor for MainPage.
         public MainPage()
         {
+            // Initializes the page components.
             InitializeComponent();
+
+            // Initialize the Tasks collection.
+            Tasks = new ObservableCollection<Task>();
+
+            // Add sample tasks to the Tasks collection.
+            Tasks.Add(new Task { Name = "Task 1", Description = "Description for Task 1" });
+            Tasks.Add(new Task { Name = "Task 2", Description = "Description for Task 2" });
+            Tasks.Add(new Task { Name = "Task 3", Description = "Description for Task 3" });
+            Tasks.Add(new Task { Name = "Task 4", Description = "Description for Task 4" });
+            Tasks.Add(new Task { Name = "Task 5", Description = "Description for Task 5" });
+
+            // Set the source of data for the TasksCollectionView to the Tasks collection.
+            TasksCollectionView.ItemsSource = Tasks;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        // Event handler for the Add button click.
+        private async void OnAddButtonClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            // Navigate to the TaskPage when the Add button is clicked.
+            //await Navigation.PushAsync(new TaskPage());
         }
     }
 }
