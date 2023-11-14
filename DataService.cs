43fdde903fdb5bd4639dbe46f43b888a0faa75d8
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-14 12:15:26 -0500

    Created a centralized data service that can be used throughout the application to access a consistent list of tasks.

---
+++ DataService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CoverYourAss
+{
+    public class DataService
+    {
+        // Singleton instance
+        private static DataService _instance;
+
+        // Property to access the singleton instance
+        public static DataService Instance => _instance ??= new DataService();
+
+        // The ObservableCollection of tasks
+        public ObservableCollection<Task> Tasks { get; private set; }
+
+        // Private constructor to prevent external instantiation
+        private DataService()
+        {
+            Tasks = new ObservableCollection<Task>();
+            // Initialize with sample data
+            LoadSampleTasks();
+        }
+
+        private void LoadSampleTasks()
+        {
+            Tasks.Add(new Task { Name = "Task 1", Description = "Description for Task 1" });
+            Tasks.Add(new Task { Name = "Task 2", Description = "Description for Task 2" });
+            Tasks.Add(new Task { Name = "Task 3", Description = "Description for Task 3" });
+            Tasks.Add(new Task { Name = "Task 4", Description = "Description for Task 4" });
+            Tasks.Add(new Task { Name = "Task 5", Description = "Description for Task 5" });
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,27 +5,14 @@ namespace CoverYourAss
     // MainPage class of the application, inheriting from ContentPage.
     public partial class MainPage : ContentPage
     {
-        // A collection that can be observed for changes. Used here to hold Task items.
-        public ObservableCollection<Task> Tasks { get; set; }
-
         // Constructor for MainPage.
         public MainPage()
         {
             // Initializes the page components.
             InitializeComponent();
 
-            // Initialize the Tasks collection.
-            Tasks = new ObservableCollection<Task>();
-
-            // Add sample tasks to the Tasks collection.
-            Tasks.Add(new Task { Name = "Task 1", Description = "Description for Task 1" });
-            Tasks.Add(new Task { Name = "Task 2", Description = "Description for Task 2" });
-            Tasks.Add(new Task { Name = "Task 3", Description = "Description for Task 3" });
-            Tasks.Add(new Task { Name = "Task 4", Description = "Description for Task 4" });
-            Tasks.Add(new Task { Name = "Task 5", Description = "Description for Task 5" });
-
-            // Set the source of data for the TasksCollectionView to the Tasks collection.
-            TasksCollectionView.ItemsSource = Tasks;
+            // Access the Tasks from the DataService
+            TasksCollectionView.ItemsSource = DataService.Instance.Tasks;
         }
 
         // Event handler for the Add button click.
