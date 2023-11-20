5a5070d8d9da7cdd1542b7952c7a1cb452eb31c0
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-20 11:51:04 -0500

    Modified the TaskPage to display the task I passed to it from the MainPage when I clicked one of the items.

--- TaskPage.xaml.cs
+++ TaskPage.xaml.cs
@@ -2,9 +2,14 @@ namespace CoverYourAss;
 
 public partial class TaskPage : ContentPage
 {
+    private Task _task;
+
     public TaskPage(Task task = null)
     {
         InitializeComponent();
+
+        _task = task ?? new Task();
+        BindingContext = _task;
     }
 
     private async void SaveTaskClicked(object sender, EventArgs e)
