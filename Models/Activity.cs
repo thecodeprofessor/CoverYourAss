e1cb510f04728f08fce01b06a245c0e1c3930551
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-27 10:22:38 -0500

    Added new models for additional functionality. Also created a new activity model to take over for the old task model.

---
+++ Models/Activity.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CoverYourAss.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public DateTime When { get; set; }
+        public Location Where { get; set; }
+        public List<Person> People { get; set; }
+        public List<string> Pictures { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<Note> Notes { get; set; }
+    }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace CoverYourAss.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
