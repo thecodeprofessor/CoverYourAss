491666043d784934dee3208ff296897cdce767f6
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-28 12:58:26 -0500

    Created a new ActivityListPage that will replace the old MainPage. This new ActivityListPage will be used to list the activities in the application and allow you to tap on them to see details. It also includes an add button.

---
+++ Views/ActivityListView.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace CoverYourAss.Views
+{
+    public partial class ActivityListView : ContentPage
+    {
+        public ActivityListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
