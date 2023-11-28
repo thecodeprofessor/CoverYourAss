1f9c317baf5f76d633bd2bff04b9f671b0bc2ab2
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-11-28 12:57:27 -0500

    Created a ViewModel for the Activity that will be used to display a selected activity in the new activity detail page. It will handle activity detail display and modifications.

---
+++ ViewModels/ActivityDetailsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CoverYourAss.Models;
+using CoverYourAss.Services;
+
+namespace CoverYourAss.ViewModels
+{
+    public class ActivityDetailsViewModel : ObservableObject
+    {
+        private Activity _activityToEdit;
+        private ActivityDataService _dataService;
+
+        public Activity ActivityToEdit
+        {
+            get => _activityToEdit;
+            set => SetProperty(ref _activityToEdit, value);
+        }
+
+        public IRelayCommand SaveActivityCommand { get; }
+        public IRelayCommand DeleteActivityCommand { get; }
+        public IRelayCommand CancelCommand { get; }
+
+        public ActivityDetailsViewModel()
+        {
+            _dataService = ActivityDataService.Instance;
+
+            SaveActivityCommand = new RelayCommand(SaveActivity);
+            DeleteActivityCommand = new RelayCommand(DeleteActivity);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+        private void SaveActivity()
+        {
+            // Save the activity
+        }
+
+        private void DeleteActivity()
+        {
+            // Delete the activity
+            _dataService.Activities.Remove(ActivityToEdit);
+        }
+
+        private void Cancel()
+        {
+            // Cancel the edit and navigate back
+        }
+    }
+}
