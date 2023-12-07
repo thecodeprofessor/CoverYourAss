6d6e1b4d7f3b439d71b709032a9567792de94e63
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-12-07 10:37:17 -0500

    Removed the task model because it was replaced with the Activity model.

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -1,17 +1,23 @@
-﻿
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System.Collections.Generic;
+
 namespace CoverYourAss.Models
 {
     public class Activity
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public string Icon { get; set; }
         public DateTime When { get; set; }
-        public Location Where { get; set; }
-        public List<Person> People { get; set; }
-        public List<string> Pictures { get; set; }
-        public List<Tag> Tags { get; set; }
-        public List<Note> Notes { get; set; }
+
+        //public List<Person> People { get; set; }
+        //public Location Where { get; set; }
+        //public List<string> Pictures { get; set; }
+        //public List<Tag> Tags { get; set; }
+        //public List<Note> Notes { get; set; }
     }
 }
