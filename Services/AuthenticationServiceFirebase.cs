5a63e76d93d140a6246286889b1c607e854138f3
Nathan Abourbih <16595468+thecodeprofessor@users.noreply.github.com>
2023-12-08 12:06:13 -0500

    Added firebase authentication with support for sign-up and sign-in. You will need to make an additional file called AppSettings.cs in your Services folder and include two public static strings: FirebaseApiKey and FirebaseAuthDomain. Also, add AppSettings.cs to your .gitignore so that your api keys are not stored in your git repository. This is a temporary method that should be replaced with another more secure method of string secrets. See Moodle for more robust methods of storing secrets.

---
+++ Services/AuthenticationServiceFirebase.cs
@@ -0,0 +1,83 @@
+﻿using CoverYourAss.Models;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Microsoft.Extensions.Configuration;
+
+namespace CoverYourAss.Services
+{
+    public class AuthenticationServiceFirebase
+    {
+        private static AuthenticationServiceFirebase _instance;
+        public static AuthenticationServiceFirebase Instance => _instance ??= new AuthenticationServiceFirebase();
+
+        private FirebaseAuthClient _client;
+
+        private AuthenticationServiceFirebase()
+        {
+        }
+
+        private void InitializeFirebase()
+        {
+            var config = new FirebaseAuthConfig
+            {
+                ApiKey = AppSettings.FirebaseApiKey,
+                AuthDomain = AppSettings.FirebaseAuthDomain,
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new EmailProvider()
+                }
+            };
+
+            _client = new FirebaseAuthClient(config);
+        }
+
+        public async Task<(bool success, string message)> SignInAsync(string email, string password)
+        {
+            InitializeFirebase();
+
+            string message;
+            var success = false;
+
+            try
+            {
+                var userCredential = await _client.SignInWithEmailAndPasswordAsync(email, password);
+                message = $"Signed in as {userCredential.User.Info.Email}";
+                success = true;
+            }
+            catch (FirebaseAuthHttpException ex)
+            {
+                message = ex.Reason.ToString() == "Unknown" ? "Incorrect email or password." : ex.Reason.ToString();
+            }
+            catch (Exception ex)
+            {
+                message = $"An error occurred: {ex.Message}";
+            }
+
+            return (success, message);
+        }
+
+        public async Task<(bool success, string message)> SignUpAsync(string email, string password)
+        {
+            InitializeFirebase();
+            string message;
+            var success = false;
+
+            try
+            {
+                var userCredential = await _client.CreateUserWithEmailAndPasswordAsync(email, password);
+                message = $"Account created for {userCredential.User.Info.Email}";
+                success = true;
+            }
+            catch (FirebaseAuthHttpException ex)
+            {
+                message = ex.Reason.ToString();
+            }
+            catch (Exception ex)
+            {
+                message = $"An error occurred: {ex.Message}";
+            }
+
+            return (success, message);
+        }
+    }
+}
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CoverYourAss.Models;
+using CoverYourAss.Services;
+
+namespace CoverYourAss.ViewModels
+{
+    public class LoginViewModel : ObservableRecipient
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        protected static AuthenticationServiceFirebase Authentication => AuthenticationServiceFirebase.Instance;
+
+        public IAsyncRelayCommand GetCommand { get; }
+        public IAsyncRelayCommand SignInCommand { get; }
+        public IAsyncRelayCommand SignUpCommand { get; }
+
+        public LoginViewModel()
+        {
+            GetCommand = new AsyncRelayCommand(Get);
+            SignInCommand = new AsyncRelayCommand(SignIn);
+            SignUpCommand = new AsyncRelayCommand<Activity>(SignUp);
+        }
+
+        private async Task Get()
+        {
+            //Activities = await Database.GetAsync<Activity>();
+            //OnPropertyChanged(nameof(Activities));
+        }
+
+        private async Task SignIn()
+        {
+            var (success, message) = await Authentication.SignInAsync(Email, Password);
+            Message = message;
+
+            if (success)
+            {
+                await Shell.Current.GoToAsync($"///{nameof(Views.ActivityListView)}");
+            }
+        }
+
+        private async Task SignUp(Activity activity)
+        {
+            var (success, message) = await Authentication.SignUpAsync(Email, Password);
+            Message = message;
+
+            if (success)
+            {
+                await Shell.Current.GoToAsync($"///{nameof(Views.ActivityListView)}");
+            }
+
+            //await Shell.Current.GoToAsync($"///{nameof(Views.ActivityDetailsView)}?id={activity.Id}");
+        }
+    }
+}
